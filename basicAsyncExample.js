6fac148c247c7c2d664e249f8d7f1126a68ec36a
Saikat Guha <sktguha@gmail.com>
2020-10-16 16:55:42 +0530

    Update basicAsyncExample.js

--- basicAsyncExample.js
+++ basicAsyncExample.js
@@ -1,5 +1,10 @@
-// Please note: selectedText is NOT the first argument, but it is a property on the first argument instead. so need to destructure to get the selectedText param,
-function basicAsyncExample({ selectedText, log }) {
+// Please note: selectedText is the first argument( a string) and the second argument is an options object containing various params like log, showError, absolutePath etc. TODO: document the options passed in the second argument
+/**
+ * 
+ * @param {String} selectedText 
+ * @param {Object} options 
+ */
+function basicAsyncExample( selectedText, { log }) {
     // example usage of log function. calling this function will show an information box in vscode
     // This actually calls vscode.window.showInformationMessage
     // NOTE: This function only takes one argument, a string
